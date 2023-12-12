f54465ec6630cbd862607ae39c5157c83139aa4a
Manoj <manojgowdacs35@gmail.com>
2023-12-12 12:57:05 +0530

    component rendering

--- Birthday-Reminder/src/App.jsx
+++ Birthday-Reminder/src/App.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
+import People from './data'
+import List from './List'
 
 const App = () => {
+
+  const [people, setPeople]  = useState(People)
+
   return (
-    <div>Birthday reminder</div>
+   <main>
+    <section className='container'>
+      <h3>{people.length} Birthdays today</h3>
+      <List people={people}/>
+      <button onClick={()=> setPeople([])}>Clear all</button>
+
+    </section>
+   </main>
   )
 }
 
