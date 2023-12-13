561069d528329ef9c32232907b1231c0bb29a587
Manoj <manojgowdacs35@gmail.com>
2023-12-13 14:33:58 +0530

    Basic Review project setup

---
+++ Reviews/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Reviews from './Reviews'
+
+const App = () => {
+  return (
+    <main>
+      <section className='container'>
+        <div className='title'>
+          <h2>Our reviews</h2>
+          <div className='underline'></div>
+        </div>
+        <Reviews />
+      </section>
+    </main>
+  )
+}
+
+export default App
