51e9f8db10ecec476075ced56a34e68b7559969c
Manoj <manojgowdacs35@gmail.com>
2023-12-18 17:46:06 +0530

    bug fixes

--- Sidebar-modal/src/App.jsx
+++ Sidebar-modal/src/App.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Home from './Home'
+import Modal from './Modal'
+import Sidebar from './Sidebar'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+    <Home />
+    <Modal />
+    <Sidebar />
+    </>
   )
 }
 
