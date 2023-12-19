f32c248e86504b337e44751f483a89b99f1da9b1
Manoj <manojgowdacs35@gmail.com>
2023-12-19 21:29:11 +0530

    navbar component

---
+++ Stripe-Menu/src/Navbar.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import logo from  './images/logo.svg'
+import {FaBars} from 'react-icons/fa'
+import { useGlobalContext } from './Context'
+
+const Navbar = () => {
+
+    const {openSidebar,openSubmenu,closeSubmenu} = useGlobalContext()
+
+    const displaySubmenu = (e) =>{
+        const page = e.target.textContent;
+        const tempBtn = e.target.getBoundingClientRect();
+        console.log(tempBtn)
+        const center = (tempBtn.left + tempBtn.right)/2
+        const bottom = tempBtn.bottom - 3
+        openSubmenu(page,{center,bottom})
+    }
+
+    const handleSubmenu = () =>{
+        closeSubmenu();
+    }
+
+  return (
+    <nav className='nav' >
+        <div className='nav-center'>
+            <div className='nav-header'>
+                <img src={logo} alt='stripe' className='nav-logo' />
+                <button className='btn toggle-btn'onClick={openSidebar} >
+                    <FaBars />
+                </button>
+            </div>
+            <ul className='nav-links'>
+                <li>
+                    <button className='link-btn' onMouseEnter={displaySubmenu} onMouseLeave={handleSubmenu}>
+                        Products
+                    </button>
+                </li>
+                <li>
+                    <button className='link-btn' onMouseOver={displaySubmenu} onMouseLeave={handleSubmenu}>
+                        Developers
+                    </button>
+                </li>
+                <li>
+                    <button className='link-btn' onMouseOver={displaySubmenu} onMouseLeave={handleSubmenu}>
+                        Company
+                    </button>
+                </li>
+            </ul>
+            <button className='btn signin-btn'>Sign in</button>
+        </div>
+
+    </nav>
+  )
+}
+
+export default Navbar
