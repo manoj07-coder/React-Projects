f85d11bdd3c7662ff2bff9f0ed1f0bb19e11d8dc
Manoj <manojgowdacs35@gmail.com>
2023-12-21 16:39:35 +0530

    modal component

---
+++ Sidebar-modal/src/Modal.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import {FaTimes} from 'react-icons/fa'
+import {useGlobalContext} from './Context'
+
+const Modal = () => {
+
+    const {isModalOpen, closeModal} = useGlobalContext()
+
+  return (
+    <div className={`${isModalOpen ? 'modal-overlay show-modal':'modal-overlay'}`}>
+        <div className='modal-container'>
+            <h3>Modal content</h3>
+            <button onClick={closeModal} className='close-modal-btn'><FaTimes /></button>
+        </div>
+
+    </div>
+  )
+}
+
+export default Modal
