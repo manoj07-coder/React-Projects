b08a133280bd55bfd8c34f53015045182191d5f7
Manoj <manojgowdacs35@gmail.com>
2023-12-21 16:41:59 +0530

    pages of the project

---
+++ Cocktails-DB/src/pages/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+        <h1 className='section-title'>About us</h1>
+        <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut molestias officiis vel dolor laborum animi necessitatibus adipisci consequuntur eaque recusandae ullam facere id commodi perferendis, ratione, vero repudiandae doloribus similique aperiam? Provident perferendis suscipit sequi aperiam nesciunt. Amet, dignissimos accusantium?
+        </p>
+    </section>
+  )
+}
+
+export default About
